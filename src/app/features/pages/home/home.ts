b635cf20181b4815badda8ffe395f3350f0faf09
Marina González Suárez <yelose@MacBook-Pro-de-Marina.local>
2025-11-18 16:56:56 +0100

    plantilla con tokens angular-material falta quitar redondez a los botones

--- src/app/features/pages/home/home.ts
+++ src/app/features/pages/home/home.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import {MatButton} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [MatButton, MatCardModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-
+  longText = `The Shiba Inu is the smallest of the six original and distinct spitz breeds of dog
+  from Japan. A small, agile dog that copes very well with mountainous terrain, the Shiba Inu was
+  originally bred for hunting.`;
 }
