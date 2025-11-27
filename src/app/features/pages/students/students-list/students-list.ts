045de1a940a0dde89ac589b715868314ff9b31f5
Marina González Suárez <yelose@MacBook-Pro-de-Marina.local>
2025-11-27 09:54:47 +0100

    showing students json format

--- src/app/features/pages/students/students-list/students-list.ts
+++ src/app/features/pages/students/students-list/students-list.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
+import { StudentService } from '../../../../core/services/student';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-students-list',
-  imports: [MatButtonModule, RouterLink],
+  imports: [MatButtonModule, RouterLink, JsonPipe],
   templateUrl: './students-list.html',
   styleUrl: './students-list.scss',
 })
 export class StudentsList {
+  private studentService = inject(StudentService)
 
+  students = this.studentService.getAllStudents()
 }
