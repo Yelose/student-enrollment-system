d7a4f626f7423932c37506c3f9dca8d2217c7f58
Marina González Suárez <yelose@MacBook-Pro-de-Marina.local>
2025-11-28 08:30:59 +0100

    students table show everything except dates, need to do a date converter from firebase

--- src/app/features/pages/students/students-list/students-list.ts
+++ src/app/features/pages/students/students-list/students-list.ts
@@ -2,11 +2,13 @@ import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { StudentService } from '../../../../core/services/student';
-import { JsonPipe } from '@angular/common';
+import { DatePipe } from '@angular/common';
+import { MatTableModule } from '@angular/material/table';
+
 
 @Component({
   selector: 'app-students-list',
-  imports: [MatButtonModule, RouterLink, JsonPipe],
+  imports: [MatButtonModule, RouterLink, DatePipe, MatTableModule],
   templateUrl: './students-list.html',
   styleUrl: './students-list.scss',
 })
@@ -14,4 +16,23 @@ export class StudentsList {
   private studentService = inject(StudentService)
 
   students = this.studentService.getAllStudents()
+
+  // TODAS las columnas
+  readonly displayedColumns: string[] = [
+    'firstName',
+    'lastName',
+    'email',
+    'phone',
+    'nationalId',
+    'address',
+    'city',
+    'province',
+    'employmentStatus',
+    'interests',
+    'createdAt',
+    'updatedAt',
+  ];
 }
+
+
+
